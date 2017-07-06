0bd71c056c69b9345a0188848d4b0e7de32dfe70
mynadahun@naver.com <mynadahun@naver.com>
2017-07-06 18:05:55 +0900

    2017.07.06 18:06

--- testbot/src/internbot.py
+++ testbot/src/internbot.py
@@ -1,5 +1,10 @@
+# -*- coding: utf-8 -*-
+
 import os
 import time
+import datetime
+import json
+import urllib2
 from slackclient import SlackClient
 
 
@@ -10,51 +15,76 @@ BOT_ID = os.environ.get("BOT_ID")
 AT_BOT = "<@" + BOT_ID + ">"
 EXAMPLE_COMMAND = "do"
 EXAMPLE_COMMAND1 = "hi"
+EXAMPLE_COMMAND2 = "alarm"
+BL = True;
 
 # instantiate Slack & Twilio clients
 slack_client = SlackClient(os.environ.get('SLACK_BOT_TOKEN'))
 
 
-def handle_command(command, channel):
-    """
-        Receives commands directed at the bot and determines if they
-        are valid commands. If so, then acts on the commands. If not,
-        returns back what it needs for clarification.
-    """
-    response = "Not sure what you mean. Use the *" + EXAMPLE_COMMAND + \
-               "* command with numbers, delimited by spaces."
-    if command.startswith(EXAMPLE_COMMAND1):
-        response = "Hello, I'm *SKUINC Intern BOT*!!"
-    if command.startswith(EXAMPLE_COMMAND):
-        response = "Sure...write some more code then I can do that!"
-    slack_client.api_call("chat.postMessage", channel=channel,
-                          text=response, as_user=True)
+def parse_slack(msg):
+    output_list = msg
+    # print(output_list)
+    # print(len(output_list))
 
-
-def parse_slack_output(slack_rtm_output):
-    """
-        The Slack Real Time Messaging API is an events firehose.
-        this parsing function returns None unless a message is
-        directed at the Bot, based on its ID.
-    """
-    output_list = slack_rtm_output
     if output_list and len(output_list) > 0:
         for output in output_list:
-            if output and 'text' in output and AT_BOT in output['text']:
-                # return text after the @ mention, whitespace removed
-                return output['text'].split(AT_BOT)[1].strip().lower(), \
-                       output['channel']
-    return None, None
+            #print(output)
 
+            if output and 'text' in output and 'BOT_ID' not in output:
+                command = output['text']
+                answer = slack_answer(command)
+
+             	if answer :
+			slack_client.api_call(
+                     		"chat.postMessage",
+                     		channel=output['channel'],
+                      		text=answer,
+				username='인턴봇',
+				as_user=True
+                	)
+
+    return None
+
+def alarm_report():
+	now = datetime.datetime.now()
+	hour = now.hour
+	min = now.minute
+	sec = now.second;
+	global BL
+	if hour == 17 and min == 55 and BL:
+		payload = {"text": "출석부를 작성하세요!!!"}
+		url = "https://hooks.slack.com/services/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"
+		req = urllib2.Request(url)
+		req.add_header('Content-Type', 'application/json')
+		urllib2.urlopen(req, json.dumps(payload))
+		BL = False
+	if hour == 18 and min == 01:
+		BL = True
+	return None
+
+
+def slack_answer(txt):
+    if txt == EXAMPLE_COMMAND1:
+        answer = "안녕하세요! 인턴봇입니다."
+    elif txt == EXAMPLE_COMMAND2:
+	payload = {"text": "웹훅 알림!!!"}
+	url = "https://hooks.slack.com/services/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"
+	req = urllib2.Request(url)
+	req.add_header('Content-Type', 'application/json')
+	urllib2.urlopen(req, json.dumps(payload))
+	return None
+    else:
+        return None
+
+    return answer
 
 if __name__ == "__main__":
-    READ_WEBSOCKET_DELAY = 1 # 1 second delay between reading from firehose
     if slack_client.rtm_connect():
-        print("StarterBot connected and running!")
+        print("Connected!")
         while True:
-            command, channel = parse_slack_output(slack_client.rtm_read())
-            if command and channel:
-                handle_command(command, channel)
-            time.sleep(READ_WEBSOCKET_DELAY)
+	    alarm_report()
+            parse_slack(slack_client.rtm_read())
+            time.sleep(1)
     else:
-        print("Connection failed. Invalid Slack token or bot ID?")
+        print("Connection failed.")
