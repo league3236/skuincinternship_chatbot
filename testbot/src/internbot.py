b8a78817d3e11358878f3c2067c39f6df3e5f2e4
mynadahun@naver.com <mynadahun@naver.com>
2017-07-11 17:31:19 +0900

    2017.07.11 17:49

--- testbot/src/internbot.py
+++ testbot/src/internbot.py
@@ -1,15 +1,19 @@
 # -*- coding: utf-8 -*-
 from __future__ import print_function
 from slackclient import SlackClient
+from urllib.parse import urlencode
+from urllib.request import Request,urlopen
+from urllib import *
+from slacker import Slacker
 
 import os
 import time
 import datetime
 import json
-import urllib2
-import httplib2
+import http.client
 import re
 import html
+import urllib
 
 from googleapiclient.http import *
 from apiclient import discovery
@@ -37,26 +41,22 @@ AT_BOT = "<@" + BOT_ID + ">"
 EXAMPLE_COMMAND = "do"
 EXAMPLE_COMMAND1 = "hi"
 EXAMPLE_COMMAND2 = "alarm"
+OJT_COMMAND = "ojt찾아줘"
 BL = True;
+slack = Slacker(os.environ.get("SLACK_BOT_TOKEN"))
 
 # instantiate Slack & Twilio clients
 slack_client = SlackClient(os.environ.get('SLACK_BOT_TOKEN'))
 
+def post_to_channel(message):
+    slack.chat.post_message('chatbot_test', message, as_user=True)
+
 def get_credentials():
-    """Gets valid user credentials from storage.
-
-    If nothing has been stored, or if the stored credentials are invalid,
-    the OAuth2 flow is completed to obtain the new credentials.
-
-    Returns:
-        Credentials, the obtained credential.
-    """
-	home_dir = os.path.expanduser('~')
-	credential_dir = os.path.join(home_dir, '.credentials')
-	if not os.path.exists(credential_dir):
+    home_dir = os.path.expanduser('~')
+    credential_dir = os.path.join(home_dir, '.credentials')
+    if not os.path.exists(credential_dir):
         os.makedirs(credential_dir)
-    credential_path = os.path.join(credential_dir,
-                                   'drive-python-quickstart.json')
+    credential_path = os.path.join(credential_dir,'drive-python-quickstart.json')
 
     store = Storage(credential_path)
     credentials = store.get()
@@ -70,45 +70,50 @@ def get_credentials():
         print('Storing credentials to ' + credential_path)
     return credentials
 
-def gdrive():
+def gdrive(keyword):
     """Shows basic usage of the Google Drive API.
 
     Creates a Google Drive API service object and outputs the names and IDs
     for up to 10 files.
     """
     #input -> unicode
-	st = '문서'
-	pre = '&#'
-	suf = ';'
-	result = ''
-	for stt in st:
-		if ord(stt) == 32:
-			result += ' '
-		else:
-    		result += (pre + str(ord(stt)) + suf)
-	credentials = get_credentials()
-	http = credentials.authorize(httplib2.Http())
-	service = discovery.build('drive', 'v3', http=http)
+    st = keyword
+    pre = '&#'
+    suf = ';'
+    result = ''
+    for stt in st:
+        if ord(stt) == 32:
+            result += ' '
+        else:
+            result += (pre + str(ord(stt)) + suf)
+    credentials = get_credentials()
+    http = credentials.authorize(httplib2.Http())
+    service = discovery.build('drive', 'v3', http=http)
 
     #results = service.files().list(
     #    pageSize=30,fields="nextPageToken, files(id, name)").execute()
     #results = service.files().list(corpora="teamDrive", includeTeamDriveItems=True, supportsTeamDrives=True, teamDriveId="0AElQsZ-ZfPD-Uk9PVA").execute()
-	results = service.files().export(fileId="1a1l_QdFvqgtKK0lO3zXeTOjnJwpvfKzs8MQQ55yxs_s", mimeType="text/html").execute(http=http)
+    results = service.files().export(fileId="1a1l_QdFvqgtKK0lO3zXeTOjnJwpvfKzs8MQQ55yxs_s", mimeType="text/html").execute(http=http)
     # print(results)
     # search Keywork in Streaming data
-	results = results.decode("utf-8")	# without this line, Printing Error!!
-	p = re.compile((u'<h[0-9] id="(.*?)"|<span style="color:#\d+;font-weight:\d+;text-decoration:none;vertical-align:baseline;font-size:\d+pt;font-family:&quot;Malgun Gothic&quot;;font-style:normal">(.*?)<\/span>'), re.UNICODE)
-	findAll = p.findall(results)
-	content = ''
-	head_id = ''
-	for i in findAll:
-		if i[0]:
-			if content and re.search(u''+result, content):
-				print("URL : " + head_id + "\n" + html.unescape(content) + "\n\n")
-			head_id = i[0]
-			content = ''
-		else :
-			content += '\n' + i[1]
+    results = results.decode("utf-8")	# without this line, Printing Error!!
+    p = re.compile((u'<h[0-9] id="(.*?)"|<span style="color:#\d+;font-weight:\d+;text-decoration:none;vertical-align:baseline;font-size:\d+pt;font-family:&quot;Malgun Gothic&quot;;font-style:normal">(.*?)<\/span>'), re.UNICODE)
+    findAll = p.findall(results)
+    content = ''
+    head_id = ''
+    count = 0
+    for i in findAll:
+        if i[0]:
+            if content and re.search(u''+result, content):
+                # print("URL : " + head_id + "\n" + html.unescape(content) + "\n\n")
+                count += 1
+                answer = "*•"+str(count)+"번쨰 검색 결과*\n"+"```"+html.unescape(content)+"\n"+"[링크] "+"https://docs.google.com/document/d/1a1l_QdFvqgtKK0lO3zXeTOjnJwpvfKzs8MQQ55yxs_s/edit#heading="+head_id+"```"
+                post_to_channel(answer)
+            head_id = i[0]
+            content = ''
+        else :
+            content += '\n' + i[1]
+    post_to_channel('총 '+str(count)+'개의 검색 결과를 찾았습니다.')
 
 def parse_slack(msg):
     output_list = msg
@@ -123,51 +128,44 @@ def parse_slack(msg):
                 command = output['text']
                 answer = slack_answer(command)
 
-             	if answer :
-			slack_client.api_call(
+                if answer :
+                    slack_client.api_call(
                      		"chat.postMessage",
                      		channel=output['channel'],
                       		text=answer,
 				username='인턴봇',
 				as_user=True
-                	)
+                )
 
     return None
 
 def alarm_report():
-	now = datetime.datetime.now()
-	hour = now.hour
-	min = now.minute
-	sec = now.second;
-	global BL
-	if hour == 17 and min == 55 and BL:
-		payload = {"username" : "주간보고서 알림", "text": "*주간 보고서를 작성하세요!!!*\nhttps://career.skuniv.ac.kr/"}
-		url = "https://hooks.slack.com/services/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"
-		req = urllib2.Request(url)
-		req.add_header('Content-Type', 'application/json')
-		urllib2.urlopen(req, json.dumps(payload))
-		BL = False
-	if hour == 18 and min == 1:
-		BL = True
-	return None
+    now = datetime.datetime.now()
+    hour = now.hour
+    min = now.minute
+    sec = now.second;
+    global BL
+    if hour == 17 and min == 55 and BL:
+        payload = {"username" : "주간보고서 알림", "text": "*주간 보고서를 작성하세요!!!*\nhttps://career.skuniv.ac.kr/"}
+        url = "https://hooks.slack.com/services/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"
+        req = urllib2.Request(url)
+        req.add_header('Content-Type', 'application/json')
+        urllib2.urlopen(req, json.dumps(payload))
+        BL = False
+    if hour == 18 and min == 1:
+        BL = True
+    return None
 
 
 def slack_answer(txt):
     if txt == EXAMPLE_COMMAND1:
         answer = "안녕하세요! 인턴봇입니다."
     elif txt == EXAMPLE_COMMAND2:
-	payload = {"username" : "주간보고서 알림", "text": "*주간 보고서*", "color" : "#36a64f", 
-		"fields" : [
-			{
-				"title" : "주간보고서 작성 알림",
-				"value" : "주간보고서를 작성하세요!!",
-			}]
-	}
-	url = "https://hooks.slack.com/services/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"
-	req = urllib2.Request(url)
-	req.add_header('Content-Type', 'application/json')
-	urllib2.urlopen(req, json.dumps(payload))
-	return None
+        post_to_channel('*알림알림*');
+        return None
+    elif txt.find(OJT_COMMAND) != -1:
+        gdrive(txt[7:])
+        return None
     else:
         return None
 
@@ -177,7 +175,7 @@ if __name__ == "__main__":
     if slack_client.rtm_connect():
         print("Connected!")
         while True:
-	    alarm_report()
+            alarm_report()
             parse_slack(slack_client.rtm_read())
             time.sleep(1)
     else:
