1bba6e9d2dd1f149f5dda9fc1f1119c6909d342e
skuinc-internship <mynadahun@naver.com>
2017-07-12 10:08:58 +0900

    2017.07.12 09:37(Windows_school)

--- gdrive/quickstart.py
+++ gdrive/quickstart.py
@@ -57,7 +57,8 @@ def main():
         print('Files:')
         for item in items:
             if item['name'].find('OJT') != -1:
-                item['name'] = item['name'].split(' ')[0]
+                item['name'] = item['name'].split(' ')[0].encode('utf-8')
+                item['id'] = item['id'].encode('utf-8')
                 print('{0} ({1})'.format(item['name'], item['id']))
             #request = service.files().get_media(fileId=item['id'])
             #fh = io.FileIO(item['name'], mode='wb')
